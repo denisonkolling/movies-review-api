b977f72104ce1ec98763c12522eaa4d445a78e99
Denison Kolling <denisonkolling@gmail.com>
2023-04-18 15:17:51 -0300

    third commit

---
+++ README.md
@@ -0,0 +1,29 @@
+<h1 align="center"> WEB API Movie Review</h1>
+
+<p align="center">
+  <a href="#-technologies">Technologies</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-project">Project</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-layout">Layout</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+</p>
+
+<br>
+
+<p align="center">
+  <img alt="cover" src="cover.jpg" width="100%">
+</p>
+
+## 🚀 Technologies
+
+- Java
+- Spring Boot
+- Mongo DB
+
+## 💻 Project
+
+This project was created to provide a backend web application for a movie review application.
+
+## 🔖 Layout
+
+You can check the project from freeCodeCamp.org [THIS_LINK](https://www.youtube.com/watch?v=5PdEmeopJVQ&t=2758s&ab_channel=freeCodeCamp.org).
+
+---
--- src/main/java/dev/deko/movies/Application.java
+++ src/main/java/dev/deko/movies/Application.java
@@ -3,17 +3,16 @@ package dev.deko.movies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @RestController
-public class MoviesApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MoviesApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 	@Bean
